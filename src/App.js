66d8b31bc1ae61d05a5cae0bc09fdbcdb12fdf6a
Rajan kumar <krcpr007@gmail.com>
2021-12-19 14:27:50 +0530

    Style modified

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  // Link
 } from "react-router-dom";
-
-import './App.css';
-import Navbar from './component/Navbar';
-import Welcome from './component/Welcome';
-import Footer from './component/Footer';
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Welcome from "./component/Welcome";
+import Footer from "./component/Footer";
 import About from "./component/About";
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-             
-           <Routes>
+         <Routes>
           <Route exact path="/" element={<Welcome />} />
-
-          <Route exact path="/about" element={<About/>} />
-            
-          
-        </Routes>
-        <Footer />
+          <Route exact path="/about" element={<About />} />
+         </Routes>
+       <Footer />
       </Router>
     </>
   );
