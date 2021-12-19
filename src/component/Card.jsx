c9c9d21726530ea3dcaff668679abb1cdc2cab07
Rajan kumar <krcpr007@gmail.com>
2021-12-19 14:26:09 +0530

    Card modified

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({Img , name, Description}) {
     return (
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card m-3 border-danger" style={{ width: "18rem" }}>
         <img style={{width:"100%"}}
           src={`${Img}/portrait_incredible.jpg`}
           className="card-img-top img-thumbnail"
