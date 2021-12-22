2ee9d8f62351229f053372e73a12f23dd61726e9
Rajan kumar <krcpr007@gmail.com>
2021-12-23 03:31:09 +0530

    tailwind css

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -2,60 +2,38 @@ import React from "react";
 
 function Carousel() {
   return (
-    <div
-      id="carouselExampleDark"
-      className="carousel carousel-fade slide"
-      data-bs-ride="carousel"
-    >
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-      <div className="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="10000">
+    <>
+      <div className="w-full carousel">
+        <div id="item1" className="w-full pt-20 carousel-item">
           <img
-            src="https://source.unsplash.com/1600x800/?marvel"
-            className="d-block w-100"
-            alt="..."
+            src="https://source.unsplash.com/daily?marvel,thor"
+            className="w-full"
+            alt="imagePic"
           />
-          <div className="carousel-caption d-none d-md-block"></div>
         </div>
-        <div className="carousel-item" data-bs-interval="2000">
+        <div id="item2" className="w-full pt-20 carousel-item">
           <img
-            src="https://source.unsplash.com/1600x800/?hulk"
-            className="d-block w-100"
-            alt="..."
+            src="https://source.unsplash.com/daily?marvel,black widow"
+            className="w-full"
+            alt="imagePic"
           />
-          <div className="carousel-caption d-none d-md-block"></div>
         </div>
-        <div className="carousel-item">
+        <div id="item3" className="w-full pt-20 carousel-item">
           <img
-            src="https://source.unsplash.com/1600x800/?super hero"
-            className="d-block w-100"
-            alt="..."
+            src="https://source.unsplash.com/daily?marvel,vision"
+            className="w-full"
+            alt="imagePic"
           />
-          <div className="carousel-caption d-none d-md-block"></div>
+        </div>
+        <div id="item4" className="w-full pt-20 carousel-item">
+          <img
+            src="https://source.unsplash.com/daily?marvel,hulk"
+            className="w-full"
+            alt="imagePic"
+          />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
