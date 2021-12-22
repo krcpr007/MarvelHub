150be509b7d2ef1cefafcbfcc44f88e373d7c4c0
Rajan kumar <krcpr007@gmail.com>
2021-12-23 03:28:59 +0530

    testing

--- src/component/Comics.jsx
+++ src/component/Comics.jsx
@@ -1,43 +1,27 @@
-import React , {useEffect, useState} from 'react'
-import Card from './Card';
+import React, { useEffect, useState } from "react";
+// import Card from "./Card";
 
-function Comics({charName}) {
+function Comics({ charName }) {
+  // const [name, setname] = useState();
+  // const [Description, setDescription] = useState();
+  // const [Img, setImg] = useState();
+  const [data, setData] = useState(JSON);
 
-    // const [name, setname] = useState();
-    // const [Description, setDescription] = useState();
-    // const [Img, setImg] = useState();
-    const [data, setData] = useState(JSON);
-    
-    useEffect(() => {
-        fetch(
-            `https://gateway.marvel.com/v1/public/comics?ts=1&title=hulk&apikey=a3938f63f5fed93b827255a83feeecd3&hash=7fe5fce16f2f9f493ac53936f10beff0`
-            )
-            .then((resp) => resp.json())
-            .then((resp) =>(
-                <>
-                  {  (setData(resp), 
-                  console.log(data))
-                    // (setname(resp.data.results[0].title ),
-                    // setDescription(resp.data.results[0].description ),
-                    // setImg(resp.data.results[0].images.path))
-                    
-                  }
-                </>
-              ));
-
-
-    }, [charName])
-
-    return (
-        <div className='container'>
-            <h2>Hiii this comics section</h2>
-            <p> lots of work are remain to do</p>
-            {/* {data.map((item)=>{
-            return    <Card  img={"img"} name={"name"} Description={"Description"}  />
-                
-            })} */}
-        </div>
+  useEffect(() => {
+    fetch(
+      `https://gateway.marvel.com/v1/public/comics?ts=1&title=hulk&apikey=a3938f63f5fed93b827255a83feeecd3&hash=7fe5fce16f2f9f493ac53936f10beff0`
     )
+      .then((resp) => resp.json())
+      .then((resp) => <>{(setData(resp), console.log(data))}</>);
+  }, [charName]);
+
+  return (
+    <div className="flex items-center w-full">
+      <h2 className="text-4xl">Hiii this comics section</h2>
+      <br />
+      <p> lots of work are remain to do</p>
+    </div>
+  );
 }
 
-export default Comics
+export default Comics;
