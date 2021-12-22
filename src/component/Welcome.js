b252a18e09470a453ff56698fcd73b8d4b36e55e
Rajan kumar <krcpr007@gmail.com>
2021-12-23 03:26:16 +0530

    temp chanegs

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -10,88 +10,64 @@ const Welcome = () => {
   const [charName, setcharName] = useState("hulk");
   const [Description, setDescription] = useState();
   const [Img, setImg] = useState();
-  const [ search , setSearch]= useState(null); 
+  const [search, setSearch] = useState(null);
   const onclickbutton = (e) => {
     e.preventDefault();
     setcharName(search);
-    
-    // fetch(
-    //   `https://gateway.marvel.com/v1/public/characters?ts=1&name=${charName || "hulk"}&apikey=a3938f63f5fed93b827255a83feeecd3&hash=7fe5fce16f2f9f493ac53936f10beff0`
-    //   )
-    //   .then((resp) => resp.json())
-    //   .then((resp) => (
-    //     <>
-    //       {
-    //         (setname(resp.data.results[0].name),
-    //         setDescription(resp.data.results[0].description),
-    //         setImg(resp.data.results[0].thumbnail.path))
-    //         // console.log(resp)
-    //       }
-    //     </>
-    //   ));
-      // https://gateway.marvel.com/v1/public/comics?ts=1&title=hulk&apikey=a3938f63f5fed93b827255a83feeecd3&hash=7fe5fce16f2f9f493ac53936f10beff0
+    // https://gateway.marvel.com/v1/public/comics?ts=1&title=hulk&apikey=a3938f63f5fed93b827255a83feeecd3&hash=7fe5fce16f2f9f493ac53936f10beff0
   };
   useEffect(() => {
-      fetch(
-        `https://gateway.marvel.com/v1/public/characters?ts=1&name=${charName || "hulk"}&apikey=a3938f63f5fed93b827255a83feeecd3&hash=7fe5fce16f2f9f493ac53936f10beff0`
-        )
-        .then((resp) => resp.json())
-        .then((resp) => (
-          <>
-            {  
-              (setname(resp.data.results[0].name),
-              setDescription(resp.data.results[0].description),
-              setImg(resp.data.results[0].thumbnail.path ))
-              
-            }
-          </>
-        ));
-  }, [charName])
+    fetch(
+      `https://gateway.marvel.com/v1/public/characters?ts=1&name=${
+        charName || "hulk"
+      }&apikey=a3938f63f5fed93b827255a83feeecd3&hash=7fe5fce16f2f9f493ac53936f10beff0`
+    )
+      .then((resp) => resp.json())
+      .then((resp) => (
+        <>
+          {
+            (setname(resp.data.results[0].name),
+            setDescription(resp.data.results[0].description),
+            setImg(resp.data.results[0].thumbnail.path))
+          }
+        </>
+      ));
+  }, [charName]);
   return (
     <>
-      <Carousel/>
+      {/* <Carousel /> */}
       {/* SearchBar  */}
       <section className="search-bar">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-10 mx-auto">
-              <form>
-                <div>
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      placeholder="hulk"
-
-                      name="search"
-                      className="form-control rounded-pill"
-                      onChange={(e) => setSearch(e.target.value)}
-                    />
-                    <div className="input-group-append">
-                      {/* here is dropdown */}
-                      <button
-                        type="submit"
-                        className="btn btn-link"
-                        onClick={onclickbutton}
-                      >
-                        <FaSearchengin />
-                      </button>
-                    </div>
-                  </div>
+        <div className="grid gird-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+          <div>
+            <form>
+              <div className="from-control">
+                <div className="relative ">
+                  <input
+                    type="text"
+                    className="w-full  pr-40 bg-gray-200 input text-black border-2 border-primary"
+                    placeholder="hulk"
+                    name="search"
+                    onChange={(e) => setSearch(e.target.value)}
+                  />
+                  <button
+                    className="absolute top-0 right-0 rounded-l-none w-36 btn border-2 border-primary"
+                    onClick={onclickbutton}
+                  >
+                    Go
+                  </button>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </section>
-      {/* <img src={backgroundImg} className="img-fluid" alt="..." /> */}
-      {/* card result */}
       <div className="">
         <div className="container">
-          <Card Img={Img} name ={name} Description= {Description}/>
+          <Card Img={Img} name={name} Description={Description} />
         </div>
-        {/* <h2>{charName.length}</h2> */}
 
-        <Comics charName={charName}/>
+        <Comics charName={charName} />
       </div>
     </>
   );
