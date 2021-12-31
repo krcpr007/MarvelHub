efd4a484ec624f5524b7d93df838c292596ed3ea
Rajan kumar <krcpr007@gmail.com>
2021-12-31 21:11:57 +0530

    updated

--- src/component/Loader.jsx
+++ src/component/Loader.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-
 function Loader() {
   return (
     <div className="border border-primary h-96 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-      <div className="animate-pulse flex space-x-4">
-        <div className="rounded-full bg-gray-700 h-10 w-10"></div>
-        <div className="flex-1 space-y-6 py-1">
+      <div className="animate-pulse  space-x-4">
+        <div className="rounded bg-gray-700 h-56 w-full"></div>
+        <div className="flex-1 space-y-6 py-10">
           <div className="h-2 bg-gray-700 rounded"></div>
           <div className="space-y-3">
             <div className="grid grid-cols-3 gap-4">
