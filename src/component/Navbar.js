9f083436ed8ee7d37cc16d395395fd8546ec9c96
Rajan kumar <krcpr007@gmail.com>
2021-12-31 21:11:37 +0530

    margin removed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className="navbar mb-12 shadow shadow-lg bg-neutral text-neutral-content">
+    <nav className="navbar mb-0 shadow shadow-lg bg-neutral text-neutral-content">
     <div className="container mx-auto">
       <div className="flex-none px-2 mx-2">
         {/* <FaGithub className="inline pr-2 text-3xl text-purple-700" /> */}
